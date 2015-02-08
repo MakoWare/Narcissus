3038ddfa0a6062747ec23a268572ff9f28dc1bbf
yintothayang <yintothayang@gmail.com>
2015-02-07 21:53:54 -0800

    init commit

---
+++ core/src/com/makoware/narcissus/Narcissus.java
@@ -0,0 +1,69 @@
+package com.makoware.narcissus;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+public class Narcissus extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+    private static final int        FRAME_COLS = 22;         // #1
+    private static final int        FRAME_ROWS = 6;         // #2
+
+    Animation                       walkAnimation;          // #3
+    Texture                         walkSheet;              // #4
+    TextureRegion[]                 walkFrames;             // #5
+    SpriteBatch                     spriteBatch;            // #6
+    TextureRegion                   currentFrame;           // #7
+
+    float stateTime;                                        // #8
+
+    @Override
+    public void create() {
+        walkSheet = new Texture(Gdx.files.internal("megamanSheetWithArmor.gif")); // #9
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS);              // #10
+        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+        walkAnimation = new Animation(0.20f, walkFrames);      // #11
+        spriteBatch = new SpriteBatch();                // #12
+        stateTime = 0f;                         // #13
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);                        // #14
+        stateTime += Gdx.graphics.getDeltaTime();           // #15
+        currentFrame = walkAnimation.getKeyFrame(stateTime, true);  // #16
+        spriteBatch.begin();
+        spriteBatch.draw(currentFrame, 50, 50);             // #17
+        spriteBatch.end();
+    }
+
+/*
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	*/
+}
