f2ad204852854c8dcbf6bd2db7b1ceb61b867cd3
yintothayang <yintothayang@gmail.com>
2015-02-14 17:26:54 -0800

    I think it may actually be working, I think the katana's joints may be too close to each other

--- core/src/com/makoware/narcissus/NarcissusGame.java
+++ core/src/com/makoware/narcissus/NarcissusGame.java
@@ -4,67 +4,89 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.makoware.rubeLoader.loader.RubeSceneLoader;
+import com.makoware.rubeLoader.rube.RubeScene;
 
 
 public class NarcissusGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    SpriteBatch batch;
+    Sprite sprite;
+    Texture img;
+    World world;
+    Body body;
 
-    private static final int        FRAME_COLS = 4;         // #1
-    private static final int        FRAME_ROWS = 3;         // #2
-
-    Animation                       walkAnimation;          // #3
-    Texture                         walkSheet;              // #4
-    TextureRegion[]                 walkFrames;             // #5
-    SpriteBatch                     spriteBatch;            // #6
-    TextureRegion                   currentFrame;           // #7
-
-    float stateTime;                                        // #8
 
     @Override
     public void create() {
-        walkSheet = new Texture(Gdx.files.internal("megaLibgdx.png")); // #9
-        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/4);              // #10
-        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        int index = 0;
-        for (int i = 0; i < FRAME_ROWS; i++) {
-            for (int j = 0; j < FRAME_COLS; j++) {
-                walkFrames[index++] = tmp[i][j];
-            }
-        }
-        walkAnimation = new Animation(0.05f, walkFrames);      // #11
-        spriteBatch = new SpriteBatch();                // #12
-        stateTime = 0f;                         // #13
+
+        RubeSceneLoader loader = new RubeSceneLoader();
+        RubeScene scene = loader.loadScene(Gdx.files.internal("basic.json"));
+        world = scene.getWorld();
+
+
+
+        batch = new SpriteBatch();
+        /*
+        img = new Texture("badlogic.jpg");
+        sprite = new Sprite(img);
+
+        // Center the sprite in the top/middle of the screen
+        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2);
+
+
+
+        world = new World(new Vector2(0, -98f), true);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        // Set our body to the same position as our sprite
+        bodyDef.position.set(sprite.getX(), sprite.getY());
+
+        // Create a body in the world using our definition
+        body = world.createBody(bodyDef);
+
+        // Now define the dimensions of the physics shape
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(sprite.getWidth()/2, sprite.getHeight()/2);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        // Shape is the only disposable of the lot, so get rid of it
+        shape.dispose();
+        */
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);                        // #14
-        stateTime += Gdx.graphics.getDeltaTime();           // #15
-        currentFrame = walkAnimation.getKeyFrame(stateTime, true);  // #16
-        spriteBatch.begin();
-        spriteBatch.draw(currentFrame, 50, 50);             // #17
-        spriteBatch.end();
+
+        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.end();
     }
 
-/*
-
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	*/
+    @Override
+    public void dispose() {
+        // Hey, I actually did some clean up in a code sample!
+        world.dispose();
+    }
 }
